2afc4b91e9fe8f8e80f2e45b24d8d431b0647ae6
盧允元 <102306029@nccu.edu.tw>
2015-04-13 17:07:01 +0800

    改gem

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+
+def index
+	@articles=Article.all
+if params[:title].present?
+	@articles=@articles.where("title like ?","%#{params[:title]}%")
+end
+if params[:content].present?
+	@articles=@articles.where("content like ?","%#{params[:content]}%")
+
+end
+end
+def show
+	@article=Article.find(params[:id])
+	
+end
+def edit
+	@article=Article.find(params[:id])
+	
+end
+def update
+	@article=Article.find(params[:id])
+	if @article.update(article_params)
+		redirect_to root_path
+	else
+		render :edit
+
+	end
+end
+
+
+def destroy
+	@article=Article.find(params[:id])
+	@article.destroy
+	redirect_to root_path
+
+end
+
+def new
+	@article=Article.new
+end
+
+def create
+	@article=Article.new(article_params)
+	if @article.save
+		redirect_to root_path
+	else
+		render :new
+	end
+
+end
+private
+def article_params
+	params.require(:article).permit(:title,:content)
+
+end
+end
